f1a52dde57daddd3064ef342fa1c25a1614fb623
alexagreisch <argreisch@dmacc.edu>
2017-05-27 16:21:30 -0500

    Added "Hello, World"

--- CreateNewFile.java
+++ CreateNewFile.java
@@ -1,1 +1,12 @@
-This is a new file that I just created and will be adding to my repo.
+//This is a new file that I just created and will be adding to my repo.
+
+
+public class CreateNewFile 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, World!");
+	}
+
+}
